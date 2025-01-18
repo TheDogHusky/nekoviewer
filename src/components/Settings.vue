e32f2ad75bc83bf98cafe02032f6292d64148a63
TheDogHusky <contact@classydev.fr>
2025-01-18 18:44:13 +0100

    feat: add settings pane

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 const settingsPaneActive = useState('settingsPaneActive');
+
+const classes = computed(() => {
+    return {
+        'settings-pane': true,
+        'active': settingsPaneActive.value
+    };
+});
+
+function toggleSettingsPane() {
+    settingsPaneActive.value = !settingsPaneActive.value;
+}
 </script>
 
 <template>
-    <div class="settings-pane" v-if="settingsPaneActive">
-        <h1>Hello settings</h1>
+    <div :class="classes">
+        <button class="close" @click="toggleSettingsPane"><Icon name="nf-md-close" /></button>
+        <h1>Settings</h1>
     </div>
 </template>
