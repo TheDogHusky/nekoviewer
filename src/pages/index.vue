1c4d00412d26e41f63048fd694abb4603e211163
TheDogHusky <contact@classydev.fr>
2025-01-21 10:10:38 +0100

    feat: removed margin under the recent h2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -62,7 +62,7 @@ const sampleMangas = [
     <main class="container">
         <h1 class="px-3">Welcome home</h1>
         <section>
-            <h2 class="px-3">Recent</h2>
+            <h2 class="px-3 mb-0">Recent</h2>
             <div class="library-container">
                 <div class="manga" v-for="manga in sampleMangas" :key="sampleMangas.indexOf(manga)">
                     <img :src="manga.image" alt="Manga" />
