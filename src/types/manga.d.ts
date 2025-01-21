6ce9b7442580de13a641963b7d246c03da98920d
TheDogHusky <contact@classydev.fr>
2025-01-21 10:11:10 +0100

    feat: started adding types

---
+++ src/types/manga.d.ts
@@ -0,0 +1,49 @@
+/**
+ * The data structure for the manga
+ */
+interface MangaData {
+    /**
+     * Manga name
+     */
+    name: string;
+    /**
+     * Manga description (synopsis)
+     */
+    description: string;
+    /**
+     * Manga author
+     */
+    author: string;
+    /**
+     * The genres of the manga
+     */
+    genres: string[];
+    /**
+     * The cover image path for the manga
+     */
+    cover: string;
+    /**
+     * The volumes of the manga
+     * Could be either some data retrieved by the electron backend (saved on the database) so it's an array of MangaVolume
+     * or just an array of strings with the volume names (retrieved by scanning for files)
+     */
+    volumes: MangaVolume[] | string[];
+}
+
+/**
+ * The data structure for a manga volume
+ */
+interface MangaVolume {
+    /**
+     * The volume name
+     */
+    name: string;
+    /**
+     * The number of pages in the volume
+     */
+    pages: number;
+    /**
+     * The volume number
+     */
+    number: number;
+}
