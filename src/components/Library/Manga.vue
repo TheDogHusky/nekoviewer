2d9b72ed143e63d6209da7b36e73524cde17c5dd
TheDogHusky <contact@classydev.fr>
2025-04-17 20:55:58 +0200

    refactor: move the library to separate components

---
+++ src/components/Library/Manga.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { cleanMangaName } from "~/utils/functions";
+
+const props = defineProps<{
+    manga: MangaData;
+}>();
+
+const { manga } = props;
+</script>
+
+<template>
+    <div class="manga">
+        <NuxtImg :src="manga.cover" placeholder width="140px" alt="Manga" />
+        <h3>{{ manga.name }}</h3>
+        <NuxtLink class="manga-btn" :to="`/manga/${cleanMangaName(manga.name)}`">Resume</NuxtLink>
+    </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,17 +12,12 @@ const recent: Ref<MangaData[] | null> = ref(await window.ipcRenderer.invoke('man
         <h1 class="px-3">Welcome back! Time for a read?</h1>
         <section>
             <h2 class="px-3 mb-0">Recent Lectures</h2>
-            <div class="library-container">
-                <div class="manga" v-if="recent" v-for="manga in recent" :key="recent.indexOf(manga)">
-                    <NuxtImg :src="manga.cover" placeholder width="140px" alt="Manga" />
-                    <h3>{{ manga.name }}</h3>
-                    <!-- TODO make a function to clean manga name for the URL -->
-                    <NuxtLink class="manga-btn" :to="`/manga/${manga.name.toLowerCase()}`">Resume</NuxtLink>
-                </div>
+            <LibraryContainer>
+                <LibraryManga v-if="recent" v-for="manga in recent" :key="recent.indexOf(manga)" :manga="manga"/>
                 <div v-else class="no-manga">
                     <p>Oops, seems like you don't have any recent lectures..</p>
                 </div>
-            </div>
+            </LibraryContainer>
         </section>
     </main>
 </template>
