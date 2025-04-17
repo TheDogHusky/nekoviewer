6f21dc856cc73177a4f95ca5b2e35f71a2fb890c
TheDogHusky <contact@classydev.fr>
2025-04-17 20:56:33 +0200

    refactor: optimization of variables

--- src/pages/manga/[manga]/index.vue
+++ src/pages/manga/[manga]/index.vue
@@ -1,40 +1,31 @@
 <script setup lang="ts">
 const route = useRoute();
 const manga = route.params.manga;
-const mangaVolumes = ref<MangaVolume[]>([]);
-const mangaCover = ref<string>('');
-const mangaData = ref<MangaData | null>(null);
 
-const data = await window.ipcRenderer.invoke('mangas:getMangaData', manga);
-mangaVolumes.value = data.volumes;
-mangaCover.value = data.coverImagePath;
-console.log(data)
-mangaData.value = data.data;
-
-console.log(mangaVolumes.value.map(volume => volume));
+const data = ref<MangaDataIPCAnswer>(await window.ipcRenderer.invoke('mangas:getMangaData', manga));
 </script>
 
 <template>
     <main class="container gap-2">
         <div class="row gap-3 strict manga-infos">
-            <NuxtImg placeholder class="manga-cover" :src="mangaCover" height="300px" alt="manga cover"/>
+            <NuxtImg placeholder class="manga-cover" :src="data.coverImagePath" height="300px" alt="manga cover"/>
             <div class="column w-auto">
-                <h1 class="capitalize">{{ manga }}</h1>
-                <p>{{ mangaData?.description }}</p>
+                <h1 class="capitalize">{{ data.data.name }}</h1>
+                <p>{{ data.data.description }}</p>
                 <h3>Genres</h3>
                 <div class="genres">
-                    <p class="genre" v-for="genre in mangaData?.genres" :key="mangaData?.genres.indexOf(genre)">{{ genre }}</p>
+                    <p class="genre" v-for="genre in data.data.genres" :key="data.data.genres.indexOf(genre)">{{ genre }}</p>
                 </div>
                 <h3>Author</h3>
-                <p class="author">{{ mangaData?.author }}</p>
+                <p class="author">{{ data.data.author }}</p>
             </div>
         </div>
         <section class="mt-2 w-100">
             <h2>Volumes</h2>
             <div class="volumes-list">
-                <div class="manga-volume" v-for="volume in mangaVolumes" v-if="mangaVolumes.length" :key="volume.number">
+                <div class="manga-volume" v-for="volume in data.volumes" v-if="data.volumes.length" :key="volume">
                     <p>Volume {{ volume }}</p>
-                    <NuxtLink class="btn btn-secondary btn-small" :to="`/manga/${manga}/volume/${volume.number}`">Read</NuxtLink>
+                    <NuxtLink class="btn btn-secondary btn-small" :to="`/manga/${manga}/volume/${volume}`">Read</NuxtLink>
                 </div>
                 <div v-else class="manga-volume no-volume">
                     <p>Oops, seems like you don't have any volumes of this manga..</p>
